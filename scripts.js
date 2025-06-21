d0bf4b8306359d64673c935c1fcb040fdb411d11
Yuri Viana <ycviana@sga.pucminas.br>
2025-06-21 09:32:01 -0300

    feat: adicionar evento que captura o evento de submit já com os valores

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
 // Seleciona os elementos do formulário
-const expense = document.querySelector('#amount')
+const form = document.querySelector('form')
+const amount = document.getElementById('amount')
+const expense = document.getElementById('expense')
+const category = document.getElementById('category')
 
 // Captura o evento de input para formatar o valor
-expense.oninput = () => {   
+amount.oninput = () => {   
     // Obtém o valor atual do input e remove os caracteres não numéricos
-    let valueExpense = expense.value.replace(/\D+/g, '')
+    let valueamount = amount.value.replace(/\D+/g, '')
 
     // Transforma o valor em centavos
-    valueExpense = Number(valueExpense) / 100
+    valueamount = Number(valueamount) / 100
 
     // Atualiza o valor do input
-    expense.value = formatCurrencyBRL(valueExpense)
+    amount.value = formatCurrencyBRL(valueamount)
 }
 
 function formatCurrencyBRL(value) {
@@ -21,4 +24,30 @@ function formatCurrencyBRL(value) {
     })
 
     return value
+}
+
+// Captura o evento de submit do formulário para obter os valores inseridos
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    // Cria um objeto com os detalhes da nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date()
+    }
+
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+    try {
+        
+    } catch (error) {
+        alert('Não foi possível adicionar a despesa a lista.')
+        console.log(error);
+    }
 }
