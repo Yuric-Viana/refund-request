bc9ad9b10fd1a94690da3b2744dea2ee6c71c6e1
Yuri Viana <ycviana@sga.pucminas.br>
2025-06-21 10:30:53 -0300

    feat: adicionar novos itens na lista

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const amount = document.getElementById('amount')
 const expense = document.getElementById('expense')
 const category = document.getElementById('category')
 
+const expenseList = document.querySelector('ul')
+
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {   
     // Obtém o valor atual do input e remove os caracteres não numéricos
@@ -43,9 +45,40 @@ form.onsubmit = (event) => {
     expenseAdd(newExpense)
 }
 
+// Adiciona um novo item na lista
 function expenseAdd(newExpense) {
     try {
-        
+        const expenseItem = document.createElement('li')
+        expenseItem.classList.add('expense')
+
+        const expenseIcon = document.createElement('img')
+        expenseIcon.setAttribute('src', `./img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute('alt', newExpense.category_name)
+
+        const expenseInfo = document.createElement('div')
+        expenseInfo.classList.add('expense-info')
+
+        const expenseName = document.createElement('strong')
+        expenseName.textContent = newExpense.expense
+
+        const expenseCategory = document.createElement('span')
+        expenseCategory.textContent = newExpense.category_name
+
+        const expenseAmount = document.createElement('span')
+        expenseAmount.classList.add('expense-amount')
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.replace('R$', '')}`
+
+        const expenseDelete = document.createElement('img')
+        expenseDelete.classList.add('remove-icon')
+        expenseDelete.src = './img/remove.svg'
+        expenseDelete.setAttribute('alt', 'remover')
+
+        expenseInfo.append(expenseName, expenseCategory)
+
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, expenseDelete)
+
+        expenseList.append(expenseItem)
+
     } catch (error) {
         alert('Não foi possível adicionar a despesa a lista.')
         console.log(error);
