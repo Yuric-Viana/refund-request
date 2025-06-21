c97a69d9452acf7c2a9ee8e28f580a68e483d3b0
Yuri Viana <ycviana@sga.pucminas.br>
2025-06-21 08:48:03 -0300

    feat: retornar o valor formatado no padrão BRL

--- scripts.js
+++ scripts.js
@@ -6,6 +6,19 @@ expense.oninput = () => {
     // Obtém o valor atual do input e remove os caracteres não numéricos
     let valueExpense = expense.value.replace(/\D+/g, '')
 
+    // Transforma o valor em centavos
+    valueExpense = Number(valueExpense) / 100
+
     // Atualiza o valor do input
-    expense.value = valueExpense
+    expense.value = formatCurrencyBRL(valueExpense)
+}
+
+function formatCurrencyBRL(value) {
+    // Formata o valor no padrão BRL (real brasileiro)
+    value = value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+
+    return value
 }
